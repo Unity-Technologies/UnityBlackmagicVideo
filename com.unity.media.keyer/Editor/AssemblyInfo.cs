1662ac502dcd6d31040ac39a738bcf6268d52562
dan-farrow <dan.farrow@unity3d.com>
2023-10-16 07:04:24 -0400

    Remove com.unity.media.keyer package.

--- com.unity.media.keyer/Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Media.Keyer.Tests.Editor")]
--- com.unity.media.keyer/Tests/Runtime/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Media.Keyer.Tests.Editor")]
-[assembly: InternalsVisibleTo("Unity.Media.Keyer.Geometry.Demo")]
